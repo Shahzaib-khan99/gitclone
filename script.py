6f66619f67aceef9513e90435805c057bc52cd7d
Shahzaib Khan <aikingshahzaib0011277711@gmail.com>
2026-08-20 23:41:03 +0500

    Fix formatting of section separators in script.py

--- script.py
+++ script.py
@@ -152,9 +152,9 @@ def make_commits_from_pattern(year):
     git_push()
 
 
-# --------------------------------
+# ---------------------------------
 # Entry Point
-# --------------------------------
+# ---------------------------------
 if __name__ == "__main__":
     loading_animation(3)
     show_start_credit()
