568d4cda2052784e6bdb8e02c3221ad7184b33fa
Shahzaib Khan <aikingshahzaib0011277711@gmail.com>
2026-08-28 08:58:01 +0500

    Fix comment formatting in git_commit function

--- script.py
+++ script.py
@@ -83,7 +83,7 @@ Made with ❤️  by Aura Farmer
 
 
 # -----------------------------------
-# Git Commit (FIXED)
+# {Git Commit (FIXED)}
 # -----------------------------------
 def git_commit(message, commit_date):
     subprocess.run(["git", "add", FILE_PATH], check=True)
